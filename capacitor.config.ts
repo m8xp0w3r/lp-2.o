7078509b3178c786c8ef4de3a0f49acbe3463c7e
Nils Leistner <nils.leistner@cn-consult.eu>
2024-09-12 09:45:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.m8xp0w3r.latschipansch',
+  appName: 'latschi-pansch',
+  webDir: 'www'
+};
+
+export default config;
