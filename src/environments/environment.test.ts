6757acdb36e8834d78cc2547651d064ca3fb68c3
Nils Leistner <nils.leistner@cn-consult.eu>
2024-10-09 22:04:16 +0200

    test release v2

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  firebase: {
+    projectId: 'latschi-pansch-aac2a',
+    appId: '1:953003772894:web:d3d1d4fd0d496e94d8eb4f',
+    storageBucket: 'latschi-pansch-aac2a.appspot.com',
+    apiKey: 'AIzaSyB00Cmkg1n_5-XDAjey6ReK2NVpCHpMIog',
+    authDomain: 'latschi-pansch-aac2a.firebaseapp.com',
+    messagingSenderId: '953003772894',
+  },
+  production: true,
+  testMode: true,
+  latschiPanschCollectionName: "panschTest",
+  loginName: "nilsleistner@me.com",
+  loginPassword: "latschi11",
+  localDevMode: false
+};
